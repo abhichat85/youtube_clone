e26e35fa6f7317932c815f8f9b111b38b211c4f0
Abhishek Chatterjee <chatterjee.85@gmail.com>
2023-02-11 06:38:22 +0530

    Push to github

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '1689153c3bmshb5afd1acc56b800p17b8dcjsnc55acfa96084',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
